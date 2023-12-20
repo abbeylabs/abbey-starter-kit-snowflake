bc4e5de7d83a0f994c3420efb63e989401dcdc94
Jeff Chao <jeffchao@me.com>
2023-12-20 15:06:36 -0800

    Use new references API, update TF provider.

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    abbey = {
+      source = "abbeylabs/abbey"
+      version = "~> 0.2.6"
+    }
+
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "~> 0.56.5"
+    }
+  }
+}
